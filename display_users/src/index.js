0edcecc47feb1405be0536f78ce4ec868894b441
Patrick Puga <patrick.puga@epic.gov>
2020-11-19 04:22:09 -0700

    Removed unused packages

--- display_users/src/index.js
+++ display_users/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
 import './styles/main.scss';
 import App from './App';
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
